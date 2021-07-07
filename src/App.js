2d9963a81e1a0dadceb9bdfd4a2dcddd069e3405
Xiaofei Cao <xiaofeicao0@gmail.com>
2021-07-07 09:39:56 -0500

    frontend show missions and QR code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/ReVo_webtest/Missions")
+        .then(res => res.json())
+        .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                items: result
+              });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+        )
+  }
+
+
+  Mission(props) {
+
+    return
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+
+      return (
+          <ul>
+            {items.map(item => (
+
+                <li>
+                  <a href={"http://localhost:8080/ReVo_webtest/MissionQRCode?missionCode="+item.MissionCode}>
+                  {item.MissionName}, Capacity: {item.MissionCapacity} </a>
+
+                </li>
+            ))}
+          </ul>
+      );
+    }
+  }
+
+
 }
-
-
 
 
 export default App;
